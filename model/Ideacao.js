6e4f91757f985865d118f45f146716226a5b710e
Aladim2017@ <ariene_silva@hotmail.com>
2019-10-30 11:20:13 -0200

    creating router for ideacao page

--- model/Ideacao.js
+++ model/Ideacao.js
@@ -3,8 +3,9 @@ var Schema = mongoose.Schema;
 
 // Define collection and schema for Solucao
 var ideacao = new Schema({
-    ideacao1: String,
-    ideacao2: String
+    ideiaIdeacao: String,
+    tipoIdeacao: String,
+    clienteId: String
 
 });
 
