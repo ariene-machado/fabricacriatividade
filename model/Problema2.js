7b9b37e788784c8d005d95be5aa7cf23260d88b7
Aladim2017@ <ariene_silva@hotmail.com>
2019-10-31 13:21:25 -0200

    creating a new problem page

--- model/Problema2.js
+++ model/Problema2.js
@@ -5,12 +5,7 @@ var Schema = mongoose.Schema;
 // Define collection and schema for Problema
 var Problema2Schema = new Schema({
     problema2: String,
-    clienteId: String,
-    userId :{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Cliente'
-    }
-
+    clienteId: String
 });
 
 module.exports = mongoose.model('Problema2', Problema2Schema);
